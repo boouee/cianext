e34ecab67eda7bd75854ca627ff3643860bfb53c
boouee <104202645+boouee@users.noreply.github.com>
2025-02-15 18:24:01 +0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -68,7 +68,7 @@ async def get_leads(client, page):
        'start' : int(page) * 50 - 50
     }
     data = json.dumps(data)
-    response = await client.post(url + 'crm.lead.list.json', headers=headers, data=data)
+    response = await client.post(url + 'crm.lead.list', headers=headers, data=data)
     response_content = response.content
     print(f"Response content: {response_content}")
     if response.status_code == 204:
