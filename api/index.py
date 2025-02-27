80b92770e3d80e19493be2a91a0c144f4c85447a
boouee <104202645+boouee@users.noreply.github.com>
2025-02-27 17:42:26 +0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -37,7 +37,7 @@ async def check_mail(client):
     result, data = imap.uid('search', None, "UNSEEN")
     if result == 'OK':
         response = await client.post("https://mdevelopeur.retailcrm.ru/api/v5/orders/create?apiKey=nHY0H7zd7UWwcEiwN0EbwhXz2eGY9o9G",
-    headers={"Content-Type": "application/x-www-form-urlencoded"},
+    headers={"Content-Type": "application/x-www-form-urlencoded"}, data={}
 )
         for num in data[0].split():
             result, data = imap.uid('fetch', num, '(RFC822)')
